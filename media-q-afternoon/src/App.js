4824089258d2ff209df55b49f4c10651bb9919a8
Austin Randall <randall@Randall.local>
2018-01-08 16:15:52 -0700

    top done

--- media-q-afternoon/src/App.js
+++ media-q-afternoon/src/App.js
@@ -1,22 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+      this.state ={
+        isAnimOn1: false
+      
+    }
+  }
+
+  toggleMenu() {
+    this.setState({
+      isAnimOn1: !this.state.isAnimOn1
+    })
+  }
+
   render() {
     return (
       <div className="App">
+      {/* this is everything inside the picture */}
       <div className='top-section'>
-        <div className='nav'>
-          <div className='nav-title'>
-            Start Bootstrap
+        {/* this is just nav bar */}
+          <div className='nav'>
+            <div className='nav-title'>
+              Start Bootstrap
+            </div>
+            <ul className='menu-text'>
+              <li className='list'>SERVICES </li>
+              <li className='list'> PORTFOLIO </li>
+              <li className='list'> ABOUT </li>
+              <li className='list'> TEAM </li>
+              <li className='list'> CONTACT</li>
+            </ul>
+              <button className='nav-btn' onClick={() => this.toggleMenu()}>Menu</button>
+          {/* this is the menu that slides out */}
           </div>
-          <div className='menu-text'>
-            <span>SERVICES </span>
-            <span> PORTFOLIO </span>
-            <span> ABOUT </span>
-            <span> TEAM </span>
-            <span> CONTACT</span>
+          <ul className={ this.state.isAnimOn1 ? 'menu-text2 shown' : 'menu-text2 hidden'}>
+              <li className='list2'>SERVICES </li>
+              <li className='list2'> PORTFOLIO </li>
+              <li className='list2'> ABOUT </li>
+              <li className='list2'> TEAM </li>
+              <li className='list2'> CONTACT</li>
+            </ul>
+        <div>
+        </div>
+        {/* This is the welcome/nice to meet you */}
+        <div className='welcome'>
+          <div className='welcome1'>
+            Welcome To Our Studio!
+          </div>
+          <div className='welcome2'>
+            IT'S NICE TO MEET YOU
+          </div>
+          <div className='welcome3'>
+            <button className='button'>TELL ME MORE</button>
           </div>
         </div>
         </div>
