564a4d5452202f66b45f968e713c39f76eb5faae
Austin Randall <randall@Randall.local>
2018-01-08 12:59:00 -0700

    initial

---
+++ media-q-afternoon/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <div className='top-section'>
+        <div className='nav'>
+          <div className='nav-title'>
+            Start Bootstrap
+          </div>
+          <div className='menu-text'>
+            <span>SERVICES </span>
+            <span> PORTFOLIO </span>
+            <span> ABOUT </span>
+            <span> TEAM </span>
+            <span> CONTACT</span>
+          </div>
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
